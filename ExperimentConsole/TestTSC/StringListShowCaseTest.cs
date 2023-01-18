b287c4f20037b0ef6471b4a728179a24621ae98c
dhanasekarmurugesan <beingDhanasekar@outlook.com>
2023-01-18 17:05:43 +0530

    Custom Generic List for Strings Is added.

---
+++ ExperimentConsole/TestTSC/StringListShowCaseTest.cs
@@ -0,0 +1,21 @@
+﻿using ExperimentConsole.TypeShowCase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentConsole.TestTSC
+{
+    public class StringListShowCaseTest
+    {
+        public static void Execute()
+        {
+            StringList strings = new StringList { "ABC", "DEF" };
+
+            Console.WriteLine($"{nameof(StringList)} contains following ({strings.Count}) entries:");
+
+            strings.ForEach(t => Console.WriteLine(t));
+        }
+    }
+}
